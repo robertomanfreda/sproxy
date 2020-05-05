b51c34398ba02935c915e508b2646590ea65069c
Roberto Manfreda <robertomanfreda1994@gmail.com>
2020-05-05 11:29:09 +0200

    [UPDATE] - banner and logs

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM adoptopenjdk/openjdk11:latest
+RUN mkdir -p /var/log/sproxy
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/robertoman/sproxy/Sproxy.java
+++ src/main/java/com/robertoman/sproxy/Sproxy.java
@@ -1,5 +1,6 @@
 package com.robertoman.sproxy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,32 +10,36 @@ import java.util.Properties;
 import static com.robertoman.sproxy.util.ANSIColors.*;
 
 @EnableConfigurationProperties
+@Slf4j
 @SpringBootApplication
 public class Sproxy {
 
     public static void main(String[] args) {
-        printBanner();
-
         SpringApplication sproxyApp = new SpringApplication(Sproxy.class);
 
         Properties properties = new Properties();
         /*https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml?&page=99*/
         properties.setProperty("server.port", "6380");
-        properties.setProperty("spring.application.name", "sproxy");
         properties.setProperty("spring.main.banner-mode", "off");
+        properties.setProperty("spring.application.name", "sproxy");
+        properties.setProperty("spring.output.ansi.enabled", "always");
         properties.setProperty("logging.level.org.springframework", "info");
+        properties.setProperty("logging.file.name", "/var/log/sproxy/sproxy");
+
+        sproxyApp.setLogStartupInfo(false);
         sproxyApp.setDefaultProperties(properties);
 
         sproxyApp.run(args);
+        printBanner();
     }
 
     private static void printBanner() {
-        String banner = ANSI_GREEN + "+----  +---+  +---+  +---+  *   *  \\   /" + ANSI_RESET +
+        String banner = ANSI_GREEN + "\n+----  +---+  +---+  +---+  *   *  \\   /" + ANSI_RESET +
                 ANSI_GREEN + "\n|      |   |  |   |  |   |   \\ /    \\ /" + ANSI_RESET +
                 ANSI_WHITE + "\n+---+  |---+  |---+  |   |    *      |" + ANSI_RESET +
                 ANSI_RED + "\n    |  |      | \\    |   |   / \\     |" + ANSI_RESET +
                 ANSI_RED + "\n----+  |      |  \\   +---+  *   *    |" + ANSI_RESET;
 
-        System.out.println(banner);
+        log.info(banner);
     }
 }
