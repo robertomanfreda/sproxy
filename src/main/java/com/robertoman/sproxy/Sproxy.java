865f923f0894c50395b3fd47c26f675fd22d64b4
Roberto Manfreda <robertomanfreda1994@gmail.com>
2020-05-05 10:48:28 +0200

    [UPDATE] - Use /config dor properties loading; [UPDATE] - logback.xml

--- src/main/java/com/robertoman/sproxy/Sproxy.java
+++ src/main/java/com/robertoman/sproxy/Sproxy.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import java.io.IOException;
 import java.util.Properties;
 
 import static com.robertoman.sproxy.util.ANSIColors.*;
@@ -13,25 +12,29 @@ import static com.robertoman.sproxy.util.ANSIColors.*;
 @SpringBootApplication
 public class Sproxy {
 
-    public static void main(String[] args) throws IOException {
-        String banner = ANSI_GREEN + "+----  +---+  +---+  +---+  *   *  \\   /" + ANSI_RESET +
-                        ANSI_GREEN + "\n|      |   |  |   |  |   |   \\ /    \\ /" + ANSI_RESET +
-                        ANSI_WHITE + "\n+---+  |---+  |---+  |   |    *      |" + ANSI_RESET +
-                        ANSI_RED + "\n    |  |      | \\    |   |   / \\     |" + ANSI_RESET +
-                        ANSI_RED + "\n----+  |      |  \\   +---+  *   *    |" + ANSI_RESET;
+    public static void main(String[] args) {
+        printBanner();
 
-        System.out.println(banner);
+        SpringApplication sproxyApp = new SpringApplication(Sproxy.class);
 
-        SpringApplication application = new SpringApplication(Sproxy.class);
-
-        Properties properties = com.robertoman.sproxy.util.Properties.loadProperties();
+        Properties properties = new Properties();
         /*https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml?&page=99*/
         properties.setProperty("server.port", "6380");
         properties.setProperty("spring.application.name", "sproxy");
         properties.setProperty("spring.main.banner-mode", "off");
         properties.setProperty("logging.level.org.springframework", "info");
-        application.setDefaultProperties(properties);
+        sproxyApp.setDefaultProperties(properties);
 
-        application.run(args);
+        sproxyApp.run(args);
+    }
+
+    private static void printBanner() {
+        String banner = ANSI_GREEN + "+----  +---+  +---+  +---+  *   *  \\   /" + ANSI_RESET +
+                ANSI_GREEN + "\n|      |   |  |   |  |   |   \\ /    \\ /" + ANSI_RESET +
+                ANSI_WHITE + "\n+---+  |---+  |---+  |   |    *      |" + ANSI_RESET +
+                ANSI_RED + "\n    |  |      | \\    |   |   / \\     |" + ANSI_RESET +
+                ANSI_RED + "\n----+  |      |  \\   +---+  *   *    |" + ANSI_RESET;
+
+        System.out.println(banner);
     }
 }
